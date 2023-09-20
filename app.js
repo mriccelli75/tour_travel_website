e28c27fce0e21094e9084c1ca785d207b5628772
Mike Riccelli <mriccelli@ons.org>
2023-09-20 14:53:11 -0400

    CSS and HTMl changes to make it responsive at various media widths

--- app.js
+++ app.js
@@ -8,3 +8,9 @@ Array.from(document.getElementsByTagName('input')).forEach(( e, i) => {
     }
   })
 })
+
+let menu_btn = document.getElementsByClassName('bi-three-dots')[0];
+let menu_bx = document.getElementById('menu_bx');
+menu_btn.addEventListener('click', () => {
+  menu_bx.classList.toggle('ul_active');
+})
